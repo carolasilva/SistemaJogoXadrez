5f0886579718b2e619b9647f3fea7eb74408b297
Carolina Alves <carol.alves99@live.com>
2019-12-17 09:19:24 -0300

    Handling exceptions and clearing screen

--- src/application/UI.java
+++ src/application/UI.java
@@ -71,4 +71,9 @@ class UI {
         }
         System.out.print(" ");
     }
+
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
 }
